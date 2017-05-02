ec3d2f1af70ecdd07b7e8f97ab4d844fe85cb2ab
CerberusOne <aingaran2003@gmail.com>
2017-05-01 17:56:41 -0700

    added more boxes

---
+++ TestProject (1)/Assets/duuuuude.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class duuuuude : MonoBehaviour {
+
+	// Use this for initialization
+    //hi
+	void Start () {
+
+//*********************************************
+		//this testing works!
+//*********************************************
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
